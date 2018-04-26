59c738cfe603a41936977c8c760b27500c7ce843
Sarah <sarahhale95@gmail.com>
2018-04-26 14:45:50 +0200

    form

--- afi-webb/src/components/Company.js
+++ afi-webb/src/components/Company.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
+import { FormControl, Button,ControlLabel,HelpBlock ,FormGroup} from 'react-bootstrap';
 
-export default class CompanyPage extends Component {
+export default class Company extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      value: ''
+    };
+  }
+
+  getValidationState() {
+    const length = this.state.value.length;
+    if (length > 10) return 'success';
+    else if (length > 5) return 'warning';
+    else if (length > 0) return 'error';
+    return null;
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
   render() {
     return (
-      <div>
-
-        <p>
-          Company
-
-        </p>
-      </div>
+      <form>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Working example with validation</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.value}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock>Validation is based on string length.</HelpBlock>
+        </FormGroup>
+        <Button type="submit">Submit</Button>
+      </form>
     );
   }
 }
