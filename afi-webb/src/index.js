f76004930242afda0f9ee30b3c1791b966a29ce6
Sarah <sarahhale95@gmail.com>
2018-04-26 14:25:42 +0200

    added apollo provider

--- afi-webb/src/index.js
+++ afi-webb/src/index.js
@@ -2,20 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Subscriber from './components/Subscriber'
 import Company from './components/Company'
-
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-
-
-
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://127.0.0.1:6000/graphql' }),
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <BrowserRouter>
-    <div>
-      <Route exact path={"/"} component={Subscriber}/>
-      <Route path={"/company"} component={Company}/>
-    </div>
-  </BrowserRouter>, document.getElementById('root'));
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <div>
+          <Route exact path={"/"} component={Subscriber}/>
+          <Route path={"/company"} component={Company}/>
+        </div>
+      </BrowserRouter>
+    </ApolloProvider>, document.getElementById('root'));
 registerServiceWorker();
