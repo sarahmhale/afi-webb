3512a424e83c930078437a3366be82098e869ce3
Sarah <sarahhale95@gmail.com>
2018-04-27 10:51:47 +0200

    working

--- afi-webb/src/components/Company.js
+++ afi-webb/src/components/Company.js
@@ -1,49 +1,126 @@
 import React, { Component } from 'react';
-import { FormControl, Button,ControlLabel,HelpBlock ,FormGroup} from 'react-bootstrap';
+import { FormControl, Button, ControlLabel, HelpBlock, FormGroup } from 'react-bootstrap';
+import { Mutation } from "react-apollo";
+import { CREATE_COMPANY } from '../queries/companyQuery'
 
 export default class Company extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeCorporateNumber = this.handleChangeCorporateNumber.bind(this);
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeTelephone = this.handleChangeTelephone.bind(this);
+    this.handleChangeAddress = this.handleChangeAddress.bind(this);
+    this.handleChangePostcode = this.handleChangePostcode.bind(this);
+    this.handleChangeCity = this.handleChangeCity.bind(this);
+
 
     this.state = {
-      value: ''
+
+      co_corporateNumber: '',
+      co_name: '',
+      co_telephone: '',
+      co_address: '',
+      co_postcode: '',
+      co_city: '',
+
     };
   }
 
-  getValidationState() {
-    const length = this.state.value.length;
-    if (length > 10) return 'success';
-    else if (length > 5) return 'warning';
-    else if (length > 0) return 'error';
-    return null;
+  handleChangeCorporateNumber(e) {
+    this.setState({ co_corporateNumber: e.target.value });
   }
 
-  handleChange(e) {
-    this.setState({ value: e.target.value });
+  handleChangeName(e) {
+    this.setState({ co_name: e.target.value });
+  }
+  handleChangeTelephone(e) {
+    this.setState({ co_telephone: e.target.value });
   }
 
+  handleChangeAddress(e) {
+    this.setState({ co_address: e.target.value });
+  }
+  handleChangePostcode(e) {
+    this.setState({ co_postcode: e.target.value });
+  }
+
+  handleChangeCity(e) {
+    this.setState({ co_city: e.target.value });
+  }
+  // utdelningsadress, postnummer samt ort
   render() {
     return (
-      <form>
-        <FormGroup
-          controlId="formBasicText"
-          validationState={this.getValidationState()}
-        >
-          <ControlLabel>Company Name:</ControlLabel>
-          <FormControl
-            type="text"
-            value={this.state.value}
-            placeholder="Enter Company name"
-            onChange={this.handleChange}
-          />
+      <Mutation mutation={CREATE_COMPANY}>
+        {(createCompany, { data,error }) => (
+          <form
+            onSubmit={e => {
+              let input = this.state
+              console.log(input)
+              e.preventDefault();
+              createCompany({
+                variables: {
+                  input: this.state
+                }
+              });
+            }}>
+            <FormGroup
+              controlId="formBasicText"
 
-          <FormControl.Feedback />
-          <HelpBlock>Validation is based on string length.</HelpBlock>
-        </FormGroup>
-        <Button type="submit">Submit</Button>
-      </form>
+            >
+              <ControlLabel>Name:</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.co_name}
+                placeholder="Enter name"
+                onChange={this.handleChangeName}
+              />
+              <br/>
+              <ControlLabel>Company number:</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.co_corporateNumber}
+                placeholder="Enter Company number"
+                onChange={this.handleChangeCorporateNumber}
+              />
+              <br/>
+              <ControlLabel>Phonenumber:</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.co_telephone}
+                placeholder="Enter phonenumber"
+                onChange={this.handleChangeTelephone}
+              />
+              <br/>
+              <ControlLabel>Adress:</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.co_address}
+                placeholder="Enter adress"
+                onChange={this.handleChangeAddress}
+              />
+              <br/>
+              <ControlLabel>Postcode:</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.co_postcode}
+                placeholder="Enter postcode"
+                onChange={this.handleChangePostcode}
+              />
+
+              <ControlLabel>City:</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.co_city}
+                placeholder="Enter postcode"
+                onChange={this.handleChangeCity}
+              />
+
+            </FormGroup>
+            <Button type="submit">Submit</Button>
+          </form>
+        )}
+      </Mutation>
     );
   }
 }
--- afi-webb/src/index.js
+++ afi-webb/src/index.js
@@ -13,7 +13,7 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://127.0.0.1:6000/graphql' }),
+  link: new HttpLink({ uri: 'http://130.239.229.48:3000/graphql' }),
   cache: new InMemoryCache()
 });
 
