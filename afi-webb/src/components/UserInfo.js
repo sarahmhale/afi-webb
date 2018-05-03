b238d73efc0df4bfbb30574240226e4bc13b2b31
Sarah <sarahhale95@gmail.com>
2018-05-03 11:47:36 +0200

    working on upadte after sub

--- afi-webb/src/components/UserInfo.js
+++ afi-webb/src/components/UserInfo.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 
-import {FormControl, Button, ControlLabel} from 'react-bootstrap';
+import {FormControl, Button, ControlLabel, FormGroup} from 'react-bootstrap';
 import {Mutation} from "react-apollo";
-import {UPDATE_SUBSCRIBER} from '../queries/Query'
+import {UPDATE_SUBSCRIBER,GET_SUBSCRIBER} from '../queries/Query'
 
 export default class UserInfo extends React.Component {
     constructor(props, context) {
@@ -17,7 +17,7 @@ export default class UserInfo extends React.Component {
         this.handleChangeCity=this.handleChangeCity.bind(this)
 
         this.state = {
-            su_id: '',
+            su_id: this.props.su.su_id,
             su_socialnumber:this.props.su.su_socialnumber,
             su_firstname: this.props.su.su_firstname,
             su_lastname: this.props.su.su_lastname,
@@ -31,8 +31,8 @@ export default class UserInfo extends React.Component {
         this.setState({su_id: e.target.value});
     }
 
-    showSubscriberInfo() {
-        this.setState({showForm: false});
+    showSubscriberInfo(bool) {
+        this.setState({showForm: bool});
     }
 
     handleChangeSocialnumber(e) {
@@ -42,101 +42,138 @@ export default class UserInfo extends React.Component {
         this.setState({su_firstname: e.target.value});
     }
 
+    handleChangeLastname(e) {
+        this.setState({su_lastname: e.target.value});
+    }
+    handleChangeAddress(e) {
+        this.setState({su_address: e.target.value});
+    }
+
+    handleChangePostcode(e) {
+        this.setState({su_postcode: e.target.value});
+    }
+    handleChangeCity(e) {
+        this.setState({su_city: e.target.value});
+    }
+
 
     render() {
-        return (<Mutation mutation={UPDATE_SUBSCRIBER}>
+        return (<Mutation mutation={UPDATE_SUBSCRIBER}
+          update={(cache, { data: { updateSubscriber } }) => {
+            console.log(cache)
+            const {subscribersByID} = cache.readQuery({ query: GET_SUBSCRIBER, variables:{id: this.state.su_id} });
+            console.log(cache);
+            console.log(subscribersByID);
+            console.log(updateSubscriber)
 
-            {(updateSubscriber, {data, error}) => {
-                    if (this.state.showForm) {
-                        return (<div>
-                            <p>Social Number :{this.props.su.su_socialnumber}</p>
+            cache.writeQuery({
+              query: GET_SUBSCRIBER,
+              data: { subscribersByID: updateSubscriber }
+            });
+          }}>
 
-                            <p>First Name: {this.props.su.su_firstname}</p>
+          {(updateSubscriber, {data, error}) => {
+            if (this.state.showForm) {
+              return (<div>
+                <p>Social Number :{this.props.su.su_socialnumber}</p>
 
-                            <p>Last Name:{this.props.su.su_lastname}</p>
+                <p>First Name: {this.props.su.su_firstname}</p>
 
-                            <p>Address:{this.props.su.su_address}</p>
+                <p>Last Name:{this.props.su.su_lastname}</p>
 
-                            <p>Postcode: {this.props.su.su_postcode}</p>
+                <p>Address:{this.props.su.su_address}</p>
 
-                            <p>City: {this.props.su.su_city}</p>
+                <p>Postcode: {this.props.su.su_postcode}</p>
 
-                            <Button onClick={()=>{
-                                this.showSubscriberInfo()
-                            }}>Change</Button>
+                <p>City: {this.props.su.su_city}</p>
 
-                        </div>)
-                    } else {
-                        return (
-                            <form
-                              onSubmit={e => {
-                                e.preventDefault();
-                                updateSubscriber({
-                                  variables: {
-                                    input: this.state
-                                  }
-                                });
-                              }}>
-                              <h1>Your info</h1>
-                              <FormGroup
-                                controlId="formBasicText"
+                <Button onClick={()=>{
+                  this.showSubscriberInfo(false)
+                }}>Change</Button>
 
-                              >
-                                <ControlLabel>Social Number:</ControlLabel>
-                                <FormControl
-                                  type="text"
-                                  value={this.state.su_socialnumber}
-                                  placeholder={this.state.su_socialnumber}
-                                  onChange={this.handleChangeSocialnumber}
-                                />
-                                <br/>
-                                <ControlLabel>Firstname:</ControlLabel>
-                                <FormControl
-                                  type="text"
-                                  value={this.state.su_firstname}
-                                  placeholder={this.state.su_firstname}
-                                  onChange={this.handleChangeFirstname}
-                                />
-                                <br/>
-                                <ControlLabel>Lastname:</ControlLabel>
-                                <FormControl
-                                  type="text"
-                                  value={this.state.su_lastname}
-                                  placeholder={this.state.su_lastname}
-                                  onChange={this.handleChangeLastname}
-                                />
-                                <br/>
-                                <ControlLabel>Adress:</ControlLabel>
-                                <FormControl
-                                  type="text"
-                                  value={this.state.su_address}
-                                  placeholder={this.state.su_address}
-                                  onChange={this.handleChangeAddress}
-                                />
-                                <br/>
-                                <ControlLabel>Postcode:</ControlLabel>
-                                <FormControl
-                                  type="text"
-                                  value={this.state.su_postcode}
-                                  placeholder={this.state.su_postcode}
-                                  onChange={this.handleChangePostcode}
-                                />
+              </div>)
+            } else {
+              return (
+                <form
+                  onSubmit={e => {
+                    e.preventDefault();
+                    this.showSubscriberInfo(true)
+                    updateSubscriber({
+                      variables: {
+                        input:{
+                          su_id:this.state.su_id,
+                          su_socialnumber: this.state.su_socialnumber,
+                          su_firstname:this.state.su_firstname,
+                          su_lastname:this.state.su_lastname,
+                          su_address:this.state.su_address,
+                          su_postcode:this.state.su_postcode,
+                          su_city:this.state.su_city,
+                          su_subscribtion_number:this.state.su_subscribtion_number
 
-                                <ControlLabel>City:</ControlLabel>
-                                <FormControl
-                                  type="text"
-                                  value={this.state.su_city}
-                                  placeholder={this.state.su_city}
-                                  onChange={this.handleChangeCity}
-                                />
+                        }
+                      }
+                    });
+                  }}>
+                  <h1>Your info</h1>
+                  <FormGroup
+                    controlId="formBasicText"
 
-                              </FormGroup>
-                              <Button type="submit">Submit</Button>
-                            </form>)
-                        )
-                    }
-                }
+                  >
+                    <ControlLabel>Social Number:</ControlLabel>
+                    <FormControl
+                      type="text"
+                      value={this.state.su_socialnumber}
+                      placeholder={this.state.su_socialnumber}
+                      onChange={this.handleChangeSocialnumber}
+                    />
+                    <br/>
+                    <ControlLabel>Firstname:</ControlLabel>
+                    <FormControl
+                      type="text"
+                      value={this.state.su_firstname}
+                      placeholder={this.state.su_firstname}
+                      onChange={this.handleChangeFirstname}
+                    />
+                    <br/>
+                    <ControlLabel>Lastname:</ControlLabel>
+                    <FormControl
+                      type="text"
+                      value={this.state.su_lastname}
+                      placeholder={this.state.su_lastname}
+                      onChange={this.handleChangeLastname}
+                    />
+                    <br/>
+                    <ControlLabel>Adress:</ControlLabel>
+                    <FormControl
+                      type="text"
+                      value={this.state.su_address}
+                      placeholder={this.state.su_address}
+                      onChange={this.handleChangeAddress}
+                    />
+                    <br/>
+                    <ControlLabel>Postcode:</ControlLabel>
+                    <FormControl
+                      type="text"
+                      value={this.state.su_postcode}
+                      placeholder={this.state.su_postcode}
+                      onChange={this.handleChangePostcode}
+                    />
+
+                    <ControlLabel>City:</ControlLabel>
+                    <FormControl
+                      type="text"
+                      value={this.state.su_city}
+                      placeholder={this.state.su_city}
+                      onChange={this.handleChangeCity}
+                    />
+
+                  </FormGroup>
+                  <Button type="submit">Submit</Button>
+                </form>)
+
             }
+          }
+          }
         </Mutation>)
     }
 }
--- afi-webb/src/queries/Query.js
+++ afi-webb/src/queries/Query.js
@@ -66,3 +66,34 @@ mutation updateSubscriber($input: UpdateSubscriberInput!) {
     }
   }
 `
+
+export const CREATE_SUBSCRIBER  = gql `
+  mutation  createSubscriber($input: CreateSubscriberInput!) {
+  createSubscriber(input: $input) {
+      su_id
+      su_socialnumber
+      su_firstname
+      su_lastname
+      su_address
+      su_postcode
+      su_city
+      su_subscribtion_number
+    }
+  }
+`
+
+export const GET_SUBSCRIBER  = gql `
+query subscribersByID($id: String!) {
+  subscribersByID(id: $id) {
+      su_id
+      su_socialnumber
+      su_firstname
+      su_lastname
+      su_address
+      su_postcode
+      su_city
+      su_subscribtion_number
+
+    }
+  }
+`
